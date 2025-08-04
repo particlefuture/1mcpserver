8a25cbb43bcaede0f8ff9e93cfca343ffd827836
Robert-Jia00129 <jia00129@umn.edu>
2025-08-04 19:18:25 -0400

    run server in http for deployment

--- server.py
+++ server.py
@@ -448,20 +448,20 @@ def fetch_readme(github_url: str) -> str:
 # 4. Run as a stdio MCP server
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    # # ---- Streamable HTTP server BLOCK ----
-    # asyncio.run(
-    #     mcp.run_async(
-    #         transport="streamable-http",
-    #         host="0.0.0.0",
-    #         port=os.getenv("PORT", 55000)
-    #     )
-    # )
-    # # ---- END Streamable HTTP server BLOCK ----
-
-    # ---- Standard I/O server BLOCK ----
+    # ---- Streamable HTTP server BLOCK ----
     asyncio.run(
         mcp.run_async(
-            transport="stdio",
+            transport="streamable-http",
+            host="0.0.0.0",
+            port=os.getenv("PORT", 8080)
         )
     )
-    # ---- END Standard I/O server BLOCK ----
+    # ---- END Streamable HTTP server BLOCK ----
+
+    # # ---- Standard I/O server BLOCK ----
+    # asyncio.run(
+    #     mcp.run_async(
+    #         transport="stdio",
+    #     )
+    # )
+    # # ---- END Standard I/O server BLOCK ----
